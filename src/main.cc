47d1011006ba841ae3742995c9f7087f0991ef74
Toto Lin <toto@linux.com>
2023-03-23 14:57:30 +0900

    Add run_file

--- src/main.cc
+++ src/main.cc
@@ -31,6 +31,155 @@
 // number of GPU threads per thread block //
 #define THREADS_PER_BLOCK 128
 
+
+std::string readFile(const std::string &name, std::ios::ios_base::openmode mode) {
+    std::ifstream file(name, mode);
+    if (!file.good()) [[unlikely]] throw std::runtime_error("Error reading strings file");
+
+    std::stringstream strStream;
+    strStream << file.rdbuf();
+    std::string str = strStream.str();
+
+    return str;
+}
+
+template<std::unsigned_integral ValueType>
+std::vector<ValueType> stringToSymbols(const std::string &str) {
+    std::vector<ValueType> symbols;
+    symbols.resize(str.length());
+    std::transform(str.begin(), str.end(), symbols.begin(), [] (char v) {
+        return static_cast<unsigned char>(v);
+    });
+
+    return symbols;
+}
+
+void run_file(long int num_threads, std::string fileName) {
+
+    // print column headers
+    std::cout << "\u03BB | compressed size (bytes) | ";
+#ifdef MULTI
+    std::cout << "time [multicore] (\u03BCs) | ";
+#endif
+#ifdef CUDA
+    std::cout << "time [gpu decode] (\u03BCs)";
+#endif
+    std::cout << std::endl << std::endl;
+
+    // vectors to record timings
+    std::vector<std::pair<std::string, size_t>> timings_cuda;
+    std::vector<std::pair<std::string, size_t>> timings_multicore;
+
+    std::cout << std::left << std::setw(5) << lambda << std::setfill(' ');
+
+    auto textBuf = stringToSymbols<uint8_t>(readFile(fileName));
+    auto dist = ANSTableGenerator::generate_distribution_from_buffer(
+            SEED, NUM_STATES, textBuf.data(), textBuf.size());
+
+    // create an ANS table, based on the distribution
+    auto table = ANSTableGenerator::generate_table(
+            dist.prob, dist.dist, dist.symbols, NUM_SYMBOLS,
+            NUM_STATES);
+
+    // derive an encoder table from the ANS table
+    auto encoder_table = ANSTableGenerator::generate_encoder_table(table);
+
+    // derive a decoder table from the ANS table
+    auto decoder_table
+            = ANSTableGenerator::get_decoder_table(encoder_table);
+
+    // tANS-encode the generated data using the encoder table
+    auto input_buffer = ANSEncoder::encode(
+            random_data->data(), input_size, encoder_table);
+
+    // allocate a buffer for the decoded output
+    auto output_buffer = std::make_shared<CUHDOutputBuffer>(input_size);
+
+#ifdef CUDA
+
+    // in GPU DRAM, allocate buffers for compressed input, coding table
+    // and decompressed output
+    auto gpu_in_buf
+        = std::make_shared<cuhd::CUHDGPUInputBuffer>(input_buffer);
+    auto gpu_table
+        = std::make_shared<cuhd::CUHDGPUCodetable>(decoder_table);
+    auto gpu_out_buf
+        = std::make_shared<cuhd::CUHDGPUOutputBuffer>(output_buffer);
+
+    // allocate auxiliary memory
+    auto gpu_decoder_memory = std::make_shared<cuhd::CUHDGPUDecoderMemory>(
+        input_buffer->get_compressed_size(),
+        SUBSEQUENCE_SIZE, THREADS_PER_BLOCK);
+
+    gpu_table->allocate();
+    gpu_in_buf->allocate();
+    gpu_out_buf->allocate();
+    gpu_decoder_memory->allocate();
+
+    // copy decoder table to the GPU
+    gpu_table->cpy_host_to_device();
+
+    // copy compressed input to the GPU
+    gpu_in_buf->cpy_host_to_device();
+
+    // decode the compressed data on the GPU
+    TIMER_START(timings_cuda, "gpu")
+    cuhd::CUHDGPUDecoder::decode(
+        gpu_in_buf, input_buffer->get_compressed_size(),
+        gpu_out_buf, output_buffer->get_uncompressed_size(),
+        gpu_table, gpu_decoder_memory, input_buffer->get_first_state(),
+        input_buffer->get_first_bit(), decoder_table->get_num_entries(),
+        11, SUBSEQUENCE_SIZE, THREADS_PER_BLOCK);
+    TIMER_STOP
+
+    // copy decompressed output from the GPU to the host system
+    gpu_out_buf->cpy_device_to_host();
+
+    // reverse all bytes
+    output_buffer->reverse();
+
+    // check for errors in decompressed data
+    if(cuhd::CUHDUtil::equals(random_data->data(),
+        output_buffer->get_decompressed_data().get(), input_size));
+    else std::cout << "mismatch" << std::endl;
+#endif
+
+#ifdef MULTI
+
+    // decode the compressed data with multiple CPU-threads
+    TIMER_START(timings_multicore, "multicore")
+    MulticoreDecoder::decode(SUBSEQUENCE_SIZE, num_threads,
+        input_buffer->get_compressed_size(),
+        output_buffer, input_buffer, decoder_table);
+    TIMER_STOP
+
+    // reverse all bytes
+    output_buffer->reverse();
+
+    // check for errors in decompressed data
+    if(cuhd::CUHDUtil::equals(random_data->data(),
+        output_buffer->get_decompressed_data().get(), input_size));
+    else std::cout << "mismatch" << std::endl;
+#endif
+
+    // print compressed size (bytes)
+    std::cout << std::left << std::setw(10)
+              << input_buffer->get_compressed_size() * sizeof(UNIT_TYPE)
+              << std::setfill(' ');
+
+    // print multicore runtime
+#ifdef MULTI
+    std::cout << std::left << std::setw(10)
+        << timings_multicore.at(0).second;
+#endif
+
+    // print GPU runtime
+#ifdef CUDA
+    std::cout << std::left << std::setw(10)
+        <<  timings_cuda.at(0).second << std::endl;
+#endif
+}
+
 void run(long int input_size, long int num_threads) {
 
     // print column headers
@@ -201,7 +350,9 @@ int main(int argc, char **argv) {
 	#endif
 	
 	// run the test
-    run(size, threads);
+    if (argv == 4)
+        run(size, threads);
+    else run(threads, argv[4]);
     
     return 0;
 }
